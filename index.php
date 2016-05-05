a073f354d326336803174feaddb76087c23d4e2f
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-05-05 09:15:27 -0600

    1%

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>4cornerfood</title>
+    <?php require_once("lib\head-utils.php");?>
+  </head>
+  <body>
+
+    <nav class="nav-pills nav-pills-default fixed">
+ <div class="container-fluid">
+   <!-- Brand and toggle get grouped for better mobile display -->
+   <div class="navbar-header">
+     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+       <span class="sr-only">Toggle navigation</span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+       <span class="icon-bar"></span>
+     </button>
+     <a class="navbar-brand" href="index.php">Home</a>
+   </div>
+   <!-- Collect the nav links, forms, and other content for toggling -->
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+     <ul class="nav navbar-nav">
+       <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
+       <li><a href="review.php">Review</a></li>
+     </ul>
+</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+
+  <div class="container">
+    <div class="jumbotron">
+      <h1>4CornersFood</h1>
+      <p>Welcome</p>
+    </div>
+  </div>
+
+
+<div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src=""
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src="">
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src="">
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src="">
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src="">
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="thumbnail">
+          <img src="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="lib\js\parallax.min.js"></script>
+  </body>
+</html>
---
+++ lib/head-utils.php
@@ -0,0 +1,18 @@
+<meta charset="utf-8">
+  <title>4cornersfood</title>
+
+  <script src="https://code.jquery.com/jquery-2.2.3.min.js"
+  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="
+  crossorigin="anonymous"></script>
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
+
+  <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css"/>
+
+<link rel="stylesheet" href="lib/css/style.css"/>
