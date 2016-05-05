9b52925123cd9f8a6e90bc8b0e67f288ada557ee
Jeremiah Johnson <jeremiahsir@gmail.com>
2016-05-05 15:17:36 -0600

    edit the nav

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>4cornerfood</title>
+    <title>4cornersfood</title>
     <?php require_once("lib\head-utils.php");?>
   </head>
   <body>
 
-    <nav class="navbar navbar-default fixed">
+    <nav class="nav-pills nav-pills-default fixed">
  <div class="container-fluid">
    <!-- Brand and toggle get grouped for better mobile display -->
    <div class="navbar-header">
@@ -23,7 +23,10 @@
    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
      <ul class="nav navbar-nav">
        <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
-       <li><a href="review.php">Review</a></li>
+       <li><a href="Use Cases.php">Use Cases</a></li>
+       <li><a href="WireFrame.php">WireFrame</a></li>
+       <li><a href="Mission Statement.php">MIssion Statement</a></li>
+       <li><a href="E.R.D.php">E.R.D</a></li>
      </ul>
 </div><!-- /.navbar-collapse -->
 </div><!-- /.container-fluid -->
@@ -36,44 +39,62 @@
     </div>
   </div>
 
-
-<!--<div class="container">
-    <div class="row">
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <div class="thumbnail">
-          <img src="">
+  <!-- <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1145.JPG" alt="lib\Images\DSCN1145.JPG">
+        <div class="caption">
+          <h3>Taco Box</h3>
+          <p></p>
         </div>
       </div>
     </div>
-  </div>-->
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1149.JPG" alt="lib\Images\DSCN1149.JPG">
+        <div class="caption">
+          <h3>Mikasa Japanese Cuisine</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1152.JPG" alt="lib\Images\DSCN1152.JPG">
+        <div class="caption">
+          <h3>Three Rivers Restaurant</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1155.JPG" alt="lib\Images\DSCN1155.JPG">
+        <div class="caption">
+          <h3>Three Rivers Pizzaera</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1164.JPG" alt="lib\Images\DSCN1164.JPG">
+        <div class="caption">
+          <h3>Captain D's Seafood</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="lib\Images\DSCN1165.JPG" alt="lib\Images\DSCN1165.JPG">
+        <div class="caption">
+          <h3>HomeTown Hamburgers</h3>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </div> -->
 
-</div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="lib\js\parallax.min.js"></script>
   </body>
 </html>
